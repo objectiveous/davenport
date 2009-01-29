04383e76818aa72efec80dc4fcdbcad279c2c92b
Robert Evans <objectiveous@gmail.com>
2009-01-29 09:53:51 -0200

    fixing a problem with the images path settings in the xcode project

---
+++ _site/build/Davenport.build/Debug/Unit Tests.build/Script-54F9F2220F006DE900EE414E.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#/Users/robevans/Projects/iPhone/google-toolbox-for-mac-1-5-1/UnitTesting/RunMacOSUnitTests.sh
+"${SYSTEM_DEVELOPER_DIR}/Tools/RunUnitTests"
---
+++ _site/build/Davenport.build/Release/Davenport.build/Script-544B3F590F13C23B00F8E540.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "---------------------------------------------- CUSTOM COPY SCRIPT"
+cp -r /Users/robevans/Projects/cocoa/CouchDb $CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
+

07b729b7a5ada88a680c147b8dfc3444fef71951
Robert Evans <objectiveous@gmail.com>
2009-01-29 10:04:06 -0200

    project cleanup

--- _site/build/Davenport.build/Release/Davenport.build/Script-544B3F590F13C23B00F8E540.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "---------------------------------------------- CUSTOM COPY SCRIPT"
-cp -r /Users/robevans/Projects/cocoa/CouchDb $CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
-

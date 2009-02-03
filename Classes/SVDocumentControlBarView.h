13063e1e65354853667554e3678c0d718efb9ff5
Robert Evans <objectiveous@gmail.com>
2009-02-03 10:37:44 -0200

    A better toolbar look for the DocumentView

---
+++ Classes/SVDocumentControlBarView.h
@@ -0,0 +1,16 @@
+//
+//  SVDocumentControlBarView.h
+//  Davenport
+//
+//  Created by Robert Evans on 2/3/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SVDocumentControlBarView : NSView {
+    NSImage *image;
+}
+
+@end
---
+++ Classes/SVDocumentControlBarView.m
@@ -0,0 +1,35 @@
+//
+//  SVDocumentControlBarView.m
+//  Davenport
+//
+//  Created by Robert Evans on 2/3/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import "SVDocumentControlBarView.h"
+
+
+@implementation SVDocumentControlBarView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {        
+        image = [NSImage imageNamed:@"segment1.png"];
+    }
+    return self;
+}
+
+-(void) dealloc{
+    [super dealloc];
+}
+- (void)drawRect:(NSRect)rect {
+    [super drawRect:rect];
+    
+    [image drawInRect:rect
+             fromRect:NSZeroRect 
+            operation:NSCompositeSourceOver
+             fraction:1];
+     
+}
+
+@end

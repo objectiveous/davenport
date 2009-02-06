fe94f8cd02dc7991558d89d6a7aa09faf1e04ac9
Robert Evans <objectiveous@gmail.com>
2009-02-06 18:12:47 -0200

    Descriptors for the Design Docs and Views

---
+++ Classes/SVDesignDocumentDescriptor.h
@@ -0,0 +1,16 @@
+//
+//  SVDesignDocumentDescriptor.h
+//  Davenport
+//
+//  Created by Robert Evans on 2/5/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SVAbstractDescriptor.h"
+
+@interface SVDesignDocumentDescriptor : SVAbstractDescriptor {
+
+}
+
+@end

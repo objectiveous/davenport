45856baefebbe327f01469977279dce3e4ea87cf
Robert Evans <objectiveous@gmail.com>
2009-02-13 20:12:27 -0200

    A first plugin. Only useful for testing the mechanics of discovery and loading of plugins.

---
+++ Classes/TNavContribution.h
@@ -0,0 +1,16 @@
+//
+//  TNavContribution.h
+//  DPTestPlugin
+//
+//  Created by Robert Evans on 2/13/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SVContributionNav.h"
+
+@interface TNavContribution : NSObject <SVContributionNav>{
+
+}
+
+@end
---
+++ Classes/TNavContribution.m
@@ -0,0 +1,20 @@
+//
+//  TNavContribution.m
+//  DPTestPlugin
+//
+//  Created by Robert Evans on 2/13/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import "TNavContribution.h"
+
+
+@implementation TNavContribution
+
+-(NSTreeNode*)navigationContribution{
+    NSTreeNode *contributionRoot = [[[NSTreeNode alloc] init] autorelease];
+    return contributionRoot;
+}
+
+
+@end

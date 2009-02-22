6f851f226cca57765d3ffa659ddbb36a21d692c7
Robert Evans <objectiveous@gmail.com>
2009-02-22 17:32:23 -0200

    the API now includes NSNotifaction names.

---
+++ Classes/SVConstants.h
@@ -0,0 +1,14 @@
+//
+//  SVConstants.h
+//  Davenport
+//
+//  Created by Robert Evans on 2/22/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+extern NSString * const DPContributionPluginDidLoadNavigationItemsNotification;
+extern NSString * const SV_NOTIFICATION_RUN_SLOW_VIEW;
+extern NSString * const DPLocalDatabaseNeedsRefreshNotification;
+
---
+++ Classes/SVConstants.m
@@ -0,0 +1,18 @@
+//
+//  SVConstants.m
+//  Davenport
+//
+//  Created by Robert Evans on 2/22/09.
+//  Copyright 2009 South And Valley. All rights reserved.
+//
+
+#import "SVConstants.h"
+
+/*!
+ This NSNotification constant is used by plugins to signal that they are ready to contribute 
+ navigation items to the Davenport left hand navigation. 
+ */
+
+NSString * const DPContributionPluginDidLoadNavigationItemsNotification = @"DPContributionPluginDidLoadNavigationItemsNotification";
+NSString * const SV_NOTIFICATION_RUN_SLOW_VIEW = @"SV_NOTIFICATION_RUN_SLOW_VIEW";
+NSString * const DPLocalDatabaseNeedsRefreshNotification = @"DPLocalDatabaseNeedsRefreshNotification";

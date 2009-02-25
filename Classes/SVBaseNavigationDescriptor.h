42b4a6a1f5e3bb62268dfad98f45a5e073580979
Robert Evans <objectiveous@gmail.com>
2009-02-25 11:30:01 -0200

    SV Descriptor replacement.

---
+++ Classes/SVBaseNavigationDescriptor.h
@@ -0,0 +1,30 @@
+//
+//  SVAbstractDescriptor.h
+//  
+//
+//  Created by Robert Evans on 12/27/08.
+//  Copyright 2008 South And Valley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "DPContributionNavigationDescriptor.h"
+
+@interface SVBaseNavigationDescriptor : NSObject <DPContributionNavigationDescriptor>{
+    // A label is the text show to the user. For example, in an outline view we might display a design document named _design/numberOfJazzBands as Number Of Jazz Bands
+    NSString                    *label;    
+    NSString                    *identity; // Identity is the actual resource name that a Descriptor represents. For example  _design/numberOfJazzBands
+    NSImage	                    *nodeIcon;
+    BOOL                        groupItem;
+    DPNavigationDescriptorTypes type;    
+}
+
+@property (retain) NSString *label;
+@property (retain) NSImage  *nodeIcon;
+@property (retain) NSString *identity;
+@property          BOOL      groupItem;
+@property DPNavigationDescriptorTypes type;
+
+
+-(id)initWithLabel:(NSString*)nodeLabel andIdentity:(NSString*)nodeIdentity type:(DPNavigationDescriptorTypes)aType;
+-(BOOL)isGroupItem;
+@end
---
+++ Classes/SVBaseNavigationDescriptor.m
@@ -0,0 +1,32 @@
+//
+//  SVAbstractDescriptor.m
+//  
+//
+//  Created by Robert Evans on 12/27/08.
+//  Copyright 2008 South And Valley. All rights reserved.
+//
+
+#import "SVBaseNavigationDescriptor.h"
+
+@implementation SVBaseNavigationDescriptor
+
+@synthesize label;
+@synthesize nodeIcon;
+@synthesize identity;
+@synthesize groupItem;
+@synthesize type;
+
+-(id)initWithLabel:(NSString*)nodeLabel andIdentity:(NSString*)nodeIdentity type:(DPNavigationDescriptorTypes)aType{
+    self = [super init];
+    if(self){
+        self.label = nodeLabel;
+        self.identity = nodeIdentity;
+        self.groupItem = NO;
+        self.type = aType;
+    }
+    return self;
+}
+-(BOOL)isGroupItem{
+    return self.groupItem;
+}
+@end
